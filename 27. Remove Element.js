b0f44e9ce1c5e79551f3663dfb7abec46a53b9f4
Tony Whocares <blueberry.acc@gmail.com>
2024-09-19 20:35:03 +0200

    fix 27 & 42

--- 27. Remove Element.js
+++ 27. Remove Element.js
@@ -4,7 +4,34 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    nums = nums.filter((x) => x !== val)
+    // nums = nums.filter((x) => x !== val)
+
+    // return nums.length
+
+    let count = 0
+    let li = 0
+    let ri = nums.length-1
+    let temp;
+    while (li <= ri) {
+
+        if (nums[li] === val) {
+            if (nums[ri] !== val) {
+                temp = nums[li]
+                nums[li] = nums[ri]
+                nums[ri] = temp
+                li++
+                ri--
+                count++
+                nums.pop()
+            } else {
+                nums.pop()
+                ri--
+            }
+        } else {
+            li++
+            count++
+        }
+    }
 
     return nums.length
 };
--- 42. Trapping Rain Water.js
+++ 42. Trapping Rain Water.js
@@ -3,28 +3,36 @@
  * @return {number}
  */
 var trap = function(height) {
-    // remember cur height + idx
-    // go forward until i find same or > height
-    // if found => fill everything up until the lowest of 2 points
-        // continue from cur index same algo
-    // if not found, get next height and repeat  
-    let highest = height[0]
-    let h_i = 0
+    let li = 0
+    let lmax = height[li] 
+    let ri = height.length-1
+    let rmax = height[ri] 
+    let min = 0
     let water = 0
-    for (let i = 1; i < height.length; i++) {
-        if (height[i] >= highest) {
-            if (i - h_i > 1) {
-                let min = Math.min(highest, height[i])
-                for (let j = h_i+1; j < i; j++) {
-                    water += min-height[j]
-                    height[j] = min
-                }
-            }
-            highest = height[i]
-            h_i = i
-        } 
-    }
 
+    while (li < ri) {
+        if (lmax >= rmax) {
+            ri--
+            if (height[ri] >= rmax) {
+                rmax = height[ri]
+            } 
+            min = Math.min(lmax, rmax)
+            if (height[ri] < min) {
+                water += min - height[ri]
+            }
+        } else {
+            li++
+            if (height[li] >= lmax) {
+                lmax = height[li]
+            }
+            min = Math.min(lmax, rmax)
+            if (height[li] < min) {
+                water += min - height[li]
+            }
+        }
+
+
+    }
     return water
 };
 
